889642bb97b7bfd360106f9e8f8f75488618db66
unknown <zheng.zhang1@hirain.com>
2017-03-24 11:03:19 +0800

    added by jenkins script

--- QAC/CreateQACJob.bat
+++ QAC/CreateQACJob.bat
@@ -1,7 +1,7 @@
 REM 1. Modify The Config File
 if %PRJSRCSUB% equ Y set PRJSRCSVN=%PRJSVNPATH%/src
 "%SEDLOCALPATH%\sed" -r "s#<url>PRJSRCGIT</url>#<url>%PRJSRCGIT%</url>#" "%QACLOCALPATH%\config.xml" | "%SEDLOCALPATH%\sed" -r "s#<name>master</name>#<name>*/%PRJDESBRANCH%</name>#" | "%SEDLOCALPATH%\sed" -r "s#<projectFile>.*</projectFile>#<projectFile>qac\\%QACJOBNAME%.prj</projectFile>#" | "%SEDLOCALPATH%\sed" -r "s#<qaVerifyProjectName>qavprjname</qaVerifyProjectName>#<qaVerifyProjectName>%QACJOBNAME%</qaVerifyProjectName>#" | "%SEDLOCALPATH%\sed" -r "s#<recipientList>.*</recipientList>#<recipientList>%PRJRECIPIENTS%</recipientList>#" > "%QACLOCALPATH%\%QACJOBNAME%.xml"
-"%SEDLOCALPATH%\sed" -r "s#SOURCEBRANCH=sourcebranch#SOURCEBRANCH=%PRJDESBRANCH%#" "%QACLOCALPATH%\batch\GenQACPrj.bat" > "%QACLOCALPATH%\batch\GenQACPrj.bat"
+%SEDLOCALPATH%\sed -r "s#SOURCEBRANCH=sourcebranch#SOURCEBRANCH=%PRJDESBRANCH%#" "%QACLOCALPATH%\batch\config.bat" > "%QACLOCALPATH%\batch\GenQACPrj.bat"
 
 REM 2. Create QAC Job
 "%JENKINSJAVAPATH%" -jar "%JENKINSCLIPATH%" -s %JENKINSURL% create-job %QACJOBNAME% --username %JENKINSUSER% --password %JENKINSPASS% < "%QACLOCALPATH%\%QACJOBNAME%.xml"
