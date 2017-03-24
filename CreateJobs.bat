94993f8f9adfbcae13db961fa63fda8c89cefba0
unknown <zheng.zhang1@hirain.com>
2017-03-24 10:39:30 +0800

    added by jenkins script

--- CreateJobs.bat
+++ CreateJobs.bat
@@ -1,14 +1,16 @@
 REM 1. Config
 call Config.bat
 
-REM 2. SVN Import Project
-if %GITJOB% equ P set/p GITJOB=是否上传tessy和qac配置到git(Y or N):    
-if %GITJOB% equ Y call GIT\GITImport.bat
 
-REM 3. Create QAC Job
+
+REM 2. Create QAC Job
 if %QACJOB% equ P set/p QACJOB=是否创建QAC JOB(Y or N):    
 if %QACJOB% equ Y call QAC\CreateQACJob.bat
 
-REM 4. Create TESSYJOB Job
+REM 3. Create TESSYJOB Job
 if %TESSYJOB% equ P set/p TESSYJOB=是否创建TESSY JOB(Y or N):    
 if %TESSYJOB% equ Y call TESSY\CreateTESSYJob.bat
+
+REM 4. SVN Import Project
+if %GITJOB% equ P set/p GITJOB=是否上传tessy和qac配置到git(Y or N):    
+if %GITJOB% equ Y call GIT\GITImport.bat
