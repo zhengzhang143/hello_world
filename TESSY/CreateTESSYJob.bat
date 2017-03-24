154c9c59bc249cbd1bfcbd690f5422dd20a602f2
unknown <zheng.zhang1@hirain.com>
2017-03-24 09:58:31 +0800

    added by jenkins script

--- TESSY/CreateTESSYJob.bat
+++ TESSY/CreateTESSYJob.bat
@@ -1,6 +1,6 @@
 REM 1. Modify The Config File
 if %PRJSRCSUB% equ Y set PRJSRCSVN=%PRJSVNPATH%/src
-"%SEDLOCALPATH%\sed" -r "s#<url>PRJSRCGIT</url>#<url>%PRJSRCGIT%</url>#" "%TESSYLOCALPATH%\config.xml" | "%SEDLOCALPATH%\sed" -r "s#<name>master</name>#<name>*//%PRJDESBRANCH%</name>#" | "%SEDLOCALPATH%\sed" -r "s#<recipientList>.*</recipientList>#<recipientList>%PRJRECIPIENTS%</recipientList>#" > "%TESSYLOCALPATH%\%TESSYJOBNAME%.xml"
+"%SEDLOCALPATH%\sed" -r "s#<url>PRJSRCGIT</url>#<url>%PRJSRCGIT%</url>#" "%TESSYLOCALPATH%\config.xml" | "%SEDLOCALPATH%\sed" -r "s#<name>master</name>#<name>*/%PRJDESBRANCH%</name>#" | "%SEDLOCALPATH%\sed" -r "s#<recipientList>.*</recipientList>#<recipientList>%PRJRECIPIENTS%</recipientList>#" > "%TESSYLOCALPATH%\%TESSYJOBNAME%.xml"
 
 REM 2. Create TESSY Job
 "%JENKINSJAVAPATH%" -jar "%JENKINSCLIPATH%" -s %JENKINSURL% create-job %TESSYJOBNAME% --username %JENKINSUSER% --password %JENKINSPASS% < "%TESSYLOCALPATH%\%TESSYJOBNAME%.xml"
